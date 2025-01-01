2185b1eb8445bbdc1da2d9f9a40319bf7902cde9
Jordan Millett <JelloJordan3@gmail.com>
2024-12-31 17:38:06 -0800

    Update

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:5147")  // Specify the allowed origin
+        policy.WithOrigins("https://jordanmillett.github.io", "http://localhost:5147")  // Specify the allowed origin
               .AllowAnyHeader()                    // Allow any headers
               .AllowAnyMethod();                   // Allow any HTTP method (GET, POST, etc.)
     });
